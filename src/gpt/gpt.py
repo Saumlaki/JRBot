2c27f2b5b59dfe1737a34e79c6ecbb29deac3feb
Alex <int_o@mail.ru>
2025-03-20 22:01:40 +0300

    1

--- src/gpt/gpt.py
+++ src/gpt/gpt.py
@@ -4,7 +4,7 @@ from langchain_gigachat.chat_models import GigaChat
 
 class gpt:
     def __init__(self, behavior : str):
-        self.giga = GigaChat(credentials="OWYwMjBlN2ItOTExYi00ZjI3LTgwZmEtN2UzOTQ3MGQyNzU0OjU1OGNjNGRmLTkzOGQtNDBiMC05OGMwLTVmMWI3N2U0NzM0OA==", verify_ssl_certs = False)
+        self.giga = GigaChat(credentials="ххххххххх", verify_ssl_certs = False)
         self.message = [SystemMessage(content=behavior)]
 
     def get_answer(self, content : str):
